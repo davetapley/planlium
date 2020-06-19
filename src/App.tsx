19f26649f2d9fe9efe1303c94fb01d998e04f4c8
Dave Tapley <dukedave@gmail.com>
2020-06-18 23:03:42 -0700

    Click positions

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
-import logo from "./logo.svg";
 import "./App.css";
+import { LeafletMouseEvent, LatLng } from "leaflet";
 
-function App() {
-  const position = { lat: 33.448, lng: -112.074 };
+const App = () => {
+  const center = new LatLng(33.448, -112.074);
+
+  const [position, setPosition] = useState<LatLng>(center);
+
+  const onClick = (event: LeafletMouseEvent): void => {
+    const { latlng } = event;
+    setPosition(latlng);
+  };
+
   return (
-    <Map center={position} zoom={13}>
+    <Map center={center} zoom={13} onClick={onClick}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
       />
       <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
+        <Popup>This is {position.toString()}</Popup>
       </Marker>
     </Map>
   );
-}
+};
 
 export default App;
