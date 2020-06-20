06f8bf560120643f35416eda8e73fc59e3d66283
Dave Tapley <dukedave@gmail.com>
2020-06-19 17:31:50 -0700

    Show distance as opacity

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,30 @@ import { Map, TileLayer, Marker, Popup, Polyline } from "react-leaflet";
 import "./App.css";
 import { LeafletMouseEvent, LatLng } from "leaflet";
 
+const perms = (n: number): number[][] => {
+  const out: number[][] = [];
+  for (var i = 0; i < n; i++) {
+    for (var j = i + 1; j < n; j++) {
+      out.push([i, j]);
+    }
+  }
+  return out;
+};
+
+const opacity = (x: LatLng, y: LatLng): number => {
+  const distance = x.distanceTo(y);
+  const minDist = 500;
+  const maxDist = 3000;
+
+  if (distance < minDist) {
+    return 0;
+  } else if (distance > maxDist) {
+    return 0;
+  }
+
+  return 1 - (distance - minDist) / (maxDist - minDist);
+};
+
 const App = () => {
   const center = new LatLng(33.448, -112.074);
 
@@ -20,25 +44,26 @@ const App = () => {
   );
   const markers = positions.map(marker);
 
-  const lines = positions.map((position) => {
-    return positions
-      .filter((position1) => position != position1)
-      .map((position1) => (
-        <Polyline positions={[position, position1]}></Polyline>
-      ));
+  const lines = perms(positions.length).map(([x_, y_]) => {
+    const x = positions[x_];
+    const y = positions[y_];
+    return <Polyline positions={[x, y]} opacity={opacity(x, y)}></Polyline>;
   });
 
   console.log(lines);
+  console.log(perms(3));
 
   return (
-    <Map center={center} zoom={13} onClick={onClick}>
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
-      />
-      {markers}
-      {lines}
-    </Map>
+    <>
+      <Map center={center} zoom={13} onClick={onClick}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
+        />
+        {markers}
+        {lines}
+      </Map>
+    </>
   );
 };
 
